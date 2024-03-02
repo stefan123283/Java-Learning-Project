43656efaf79ed4697ed9a233132eb2efc74fcaa2
stefan123283 <stefanthebess@gmail.com>
2024-03-02 21:58:20 +0200

    Initial Commit - Added the Variables and Comments Task

---
+++ src/main/java/com/stefanfrunzaqa/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,45 @@
+package com.stefanfrunzaqa.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+
+        //Declare the primitive type variables
+
+        byte byteValue1;
+        byte byteValue2;
+        short shortValue1;
+        short shortValue2;
+        int intValue1;
+        int intValue2;
+        long longValue1;
+        long longValue2;
+        float floatValue1;
+        float floatValue2;
+        double doubleValue1;
+        double doubleValue2;
+        boolean falseValue;
+        boolean trueValue;
+        char charValue1;
+        char charValue2;
+
+        //Initialize the primitive type variables
+
+        byteValue1 = -128;
+        byteValue2 = 127;
+        shortValue1 = -32768;
+        shortValue2 = 32767;
+        intValue1 = -2147483648;
+        intValue2 = 2147483647;
+        longValue1 = -9223372036854775808L;
+        longValue2 = 9223372036854775807L;
+        floatValue1 = -3.4E+38F;
+        floatValue2 = 3.4E+38F;
+        doubleValue1 = -1.7E+308;
+        doubleValue2 = 1.7E+308;
+        falseValue = false;
+        trueValue = true;
+        charValue1 = 'A';
+        charValue2 = 'Z';
+
+    }
+}
